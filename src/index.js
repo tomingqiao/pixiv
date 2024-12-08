a306f360761bef00f35117894e952123101bc225
tomingqiao <3107099063@qq.com>
2024-12-08 16:21:43 +0800

    h

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-function getQueryVariable(variable)
-{
-       var query = window.location.search.substring(1);
-       var vars = query.split("&");
-       for (var i=0;i<vars.length;i++) {
-               var pair = vars[i].split("=");
-               if(pair[0] == variable){return pair[1];}
-       }
-       return(false);
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
 }
 
 var pid = getQueryVariable("pid");
 console.log(pid);
 if (pid != false) {
-    var uri = pid;
+    var api = 'https://pximg.hakurei.cc/ajax/illust/'
+    var url = api + pid
     $.ajax({
-        url: "https://pximg.hakurei.cc/ajax/illust/" + uri,
+        url: url,
         type: "GET",
         dataType: "json",
-        success: 
+        success:
             function (data) {
                 var url = data.urls.original;
-                window.location.replace(url);
-        }
+                if (url == null) {
+                    alert('色图, url为空')
+                }
+                else
+                    window.location.replace(url);
+            }
     });
 }
